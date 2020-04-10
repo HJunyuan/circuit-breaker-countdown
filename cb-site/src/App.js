47c6095a642161449ce487ea0f4a942db2d70bb4
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-10 14:08:22 +0800

    Calculate difference in days

--- cb-site/src/App.js
+++ cb-site/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import bg from "./img/background.png";
+
+import { Container } from "react-bootstrap";
+import Countdown from "./components/Countdown";
+
+const styles = {
+	root: {
+		height: "100vh",
+		background: `url(${bg}) no-repeat center center fixed`,
+		backgroundSize: "cover",
+	},
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={styles.root}>
+			<Container>
+				<Countdown />
+			</Container>
+		</div>
+	);
 }
 
 export default App;
