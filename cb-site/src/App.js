a887b1dfdbae5986ec3585e90b53de09946b0e6e
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-10 17:58:47 +0800

    Removed react-helmet

--- cb-site/src/App.js
+++ cb-site/src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-import bg from "./img/BG_Skyline.svg";
-import SEO from "./components/SEO";
-import Clouds from "./components/Clouds";
+import Background from "./components/Background";
 import Countdown from "./components/Countdown";
 import Footer from "./components/Footer";
 
 const styles = {
 	root: {
 		height: "100vh",
-		background: `url(${bg}) no-repeat center center fixed`,
-		backgroundSize: "cover",
 		textAlign: "center",
 		display: "flex",
 		flexDirection: "column",
@@ -27,9 +23,8 @@ const styles = {
 function App() {
 	return (
 		<div style={styles.root}>
-			<SEO />
 			<main style={styles.main}>
-				<Clouds />
+				<Background />
 				<Container style={styles.container}>
 					<Countdown />
 				</Container>
