397f83cabc32b9d4ace1f81aaa51c57652c9d857
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-04-10 16:23:42 +0800

    Import layers

--- cb-site/src/components/Clouds.js
+++ cb-site/src/components/Clouds.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import cloud1 from "../img/BG_Cloud1.svg";
+import cloud2 from "../img/BG_Cloud2.svg";
+import sun from "../img/BG_Sun.svg";
+
 export default (props) => {
 	return "clouds";
 };
