b9fe9375cde6a2c117ba96b503593d33a223c9c5
Guang Wei <guangwei.ngoh@gmail.com>
2020-04-10 17:33:49 +0800

    Added parallax and animations

--- cb-site/src/components/Clouds.js
+++ cb-site/src/components/Clouds.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import Parallax from "parallax-js";
+import "./Animation.css";
 
 import cloud1 from "../img/BG_Cloud1.svg";
 import cloud2 from "../img/BG_Cloud2.svg";
 import sun from "../img/BG_Sun.svg";
 
+const styles = {
+  parallax: {
+    width: "100%",
+    height: "100%",
+    position: "fixed",
+  },
+  parallaxparent: {
+    width: "100%",
+    height: "100%",
+  },
+  parallaximg: {
+    width: "110%",
+    height: "100%",
+    objectFit: "cover",
+  },
+};
+
 export default (props) => {
-	return "clouds";
+  useEffect(() => {
+    var scene = document.getElementById("scene");
+    // var parallaxInstance = new Parallax(scene);
+    new Parallax(scene);
+  }, []);
+
+  //   return "clouds";
+
+  return (
+    <>
+      <div id="scene" style={styles.parallax}>
+        <div data-depth="0.1" style={styles.parallaxparent}>
+          <img
+            src={sun}
+            alt="sun"
+            draggable={false}
+            className="parallaxchild3"
+            style={styles.parallaximg}
+          />
+        </div>
+        <div data-depth="0.2" style={styles.parallaxparent}>
+          <img
+            src={cloud1}
+            alt="cloud1"
+            draggable={false}
+            className="parallaxchild2"
+            style={styles.parallaximg}
+          />
+        </div>
+        <div data-depth="0.2" style={styles.parallaxparent}>
+          <img
+            src={cloud2}
+            alt="cloud2"
+            draggable={false}
+            className="parallaxchild"
+            style={styles.parallaximg}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
